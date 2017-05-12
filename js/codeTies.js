c2a714b7ea6f846bf58d7c0295d2fc894e57af52
Ties Noordhuis <tiesnoordhuis@gmail.com>
2017-05-12 20:59:55 +0200

    extra tekst en knoppen verkleuren

--- js/codeTies.js
+++ js/codeTies.js
@@ -7,3 +7,36 @@ $('#nav').affix({
         top: $('header').height()
       }
 });
+
+function homeActive() {
+  unActive();
+  document.getElementById("home").className = "active";
+};
+
+function kernActive() {
+  unActive();
+  document.getElementById("kern").className = "active";
+};
+
+function werkActive() {
+  unActive();
+  document.getElementById("werk").className = "active";
+};
+
+function contperActive() {
+  unActive();
+  document.getElementById("contper").className = "active";
+};
+
+function contactActive() {
+  unActive();
+  document.getElementById("contact").className = "active";
+};
+
+function unActive() {
+  document.getElementById("home").className = "";
+  document.getElementById("kern").className = "";
+  document.getElementById("werk").className = "";
+  document.getElementById("contper").className = "";
+  document.getElementById("contact").className = "";
+};
